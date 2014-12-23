44eee35b95f9c84a11767aeb96c31052b6e0a423
mattfaluotico <matt.faluotico@gmail.com>
2014-12-23 10:14:00 -0500

    i did this in the wrong project, but im sure we can use it

--- server.rb
+++ server.rb
@@ -3,3 +3,18 @@ require 'sinatra'
 get '/' do
   erb :index
 end
+
+get '/about' do
+  erb :about
+end
+
+get '/post/:postid' do
+  # erb
+end
+
+get '/signup' do
+  erb :signup
+end
+
+post '/newaccount' do
+end
